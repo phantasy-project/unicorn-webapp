b4e6bfe84e6be2fe52537228926699c4ebfc1574
Tong Zhang <zhangt@frib.msu.edu>
2017-10-12 16:41:32 -0400

    Add and rename columns.

--- app/models.py
+++ app/models.py
@@ -1,22 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 from . import db
 from .utils import utc2local
-
-
-functions = [
-    {
-        'name': 'f1',
-        'description': 'first conversion function',
-        'invoked': 0,
-        'def': """def f():\n  return 1"""
-    },
-    {
-        'name': 'f2',
-        'description': 'second conversion function',
-        'invoked': 0,
-    },
-]
 
 
 class User(db.Model):
@@ -52,11 +39,14 @@ class Function(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     invoked = db.Column(db.Integer, index=True)
-    definition = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
+    code = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime,
+                          default=datetime.utcnow,
+                          onupdate=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    desc = db.Column(db.String(100))
+    description = db.Column(db.String(100))
     args = db.Column(db.String(20))
+    lastin = db.Column(db.String(20))
     lastout = db.Column(db.String(20))
 
     def local_time(self):
