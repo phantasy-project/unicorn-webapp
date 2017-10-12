9f27ce79d8ae638a0bffa73d8fb94dd2b35db738
Tong Zhang <zhangt@frib.msu.edu>
2017-10-12 16:40:34 -0400

    Add new restful APIs, and use flask-migrate.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 import os
 
@@ -17,14 +18,18 @@ app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 api = Api(app)
 
 
-from .views import FunctionAPI, FunctionListAPI
+from .views import FunctionAPI
+from .views import FunctionListAPI
+from .views import FunctionExecAPI
 
-api.add_resource(FunctionAPI, '/api/v2.0/functions/<string:name>',
+api.add_resource(FunctionAPI, '/functions/<string:name>',
                  endpoint='function')
-api.add_resource(FunctionListAPI, '/api/v2.0/functions',
+api.add_resource(FunctionListAPI, '/functions',
                  endpoint='functions')
-# api.add_resource(FunctionListAPI, '/', endpoint='')
+api.add_resource(FunctionExecAPI, '/api/functions/<string:name>',
+                 endpoint='func')
