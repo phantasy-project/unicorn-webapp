bdfbc9fb59b397e571430124b89f0345c9661830
Tong Zhang <zhangt@frib.msu.edu>
2017-10-17 10:38:12 -0400

    Support HTTPS.

--- run.py
+++ run.py
@@ -5,5 +5,8 @@ from app import app as application
 
 
 if __name__ == '__main__':
-    application.run(threaded=True)
+    application.run(host="0.0.0.0",
+                    #ssl_context='adhoc',
+                    ssl_context=("cert.pem", "key.pem"),
+                    threaded=True,)
     #application.run()
