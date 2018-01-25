296171dac545a03d9538155f5019640da2647a87
Tong Zhang <zhangt@frib.msu.edu>
2018-01-25 11:24:15 -0500

    Create fields module to maintain marshaling.

---
+++ app/fields.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from flask_restful import fields
+
+
+function_fields = {
+    'name': fields.String,
+    'udef': fields.String(attribute=lambda x:x.udef()),
+    'code': fields.String,
+    'invoked': fields.Integer,
+    'author': fields.String(attribute=lambda x: x.author_name()),
+    'uri': fields.Url('function', absolute=True),
+    'uri-api': fields.Url('func', absolute=True),
+    'timestamp': fields.String(attribute=lambda x:x.local_time()),
+    'description': fields.String,
+    'args': fields.String,
+    'lastin': fields.String,
+    'lastout': fields.String,
+}
