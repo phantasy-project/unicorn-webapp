b1c89ed1f9774cb2692cbd1dd44d2842ee9b61c0
Tong Zhang <zhangt@frib.msu.edu>
2018-01-25 11:29:41 -0500

    Changed authentication policy.
    - use hashed password rather than password string

--- app/auth.py
+++ app/auth.py
@@ -2,18 +2,19 @@ from flask_httpauth import HTTPBasicAuth
 from flask import make_response
 from flask import jsonify
 
+from .models import User
 
 auth = HTTPBasicAuth()
 
-users = {
-    'dev': 'dev',
-}
 
-@auth.get_password
-def get_password(username):
-    if username in users:
-        return users.get(username)
-    return None
+@auth.verify_password
+def verify_password(username, password):
+    u = User.query.filter(User.nickname==username).first()
+    if not u:
+        return False
+    if u.verify_password(password):
+        return True
+    return False
 
 @auth.error_handler
 def unauthorized():
