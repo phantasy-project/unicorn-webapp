2756d3df7c942c606470ff6a9cbd61715c6b7220
Tong Zhang <zhangt@frib.msu.edu>
2018-01-25 11:34:45 -0500

    Add index view and remove testing config file.

--- app/__init__.py
+++ app/__init__.py
@@ -13,11 +13,9 @@ static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                           '../static')
 
 app = Flask(__name__,
-            instance_relative_config=True,
             template_folder=template_dir,
             static_folder=static_dir)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -25,9 +23,30 @@ migrate = Migrate(app, db)
 api = Api(app)
 
 
+from .models import Function
+from .fields import function_fields
+from flask_restful import marshal
+
+from flask import Response
+from flask import render_template
+
+@app.route('/')
+def index():
+    fs = Function.query.all()
+    #funcs = sorted([marshal(f, function_fields) for f in fs],
+    #        key=lambda x:x['invoked'], reverse=True)[:100]
+    funcs = [marshal(f, function_fields) for f in fs] # sort by dataTables.
+    return Response(
+            render_template('index.html',
+                title='Unicorn Service',
+                funcs=funcs),
+            mimetype='text/html')
+
+
 from .views import FunctionAPI
 from .views import FunctionListAPI
 from .views import FunctionExecAPI
+
 
 api.add_resource(FunctionAPI, '/functions/<string:name>',
                  endpoint='function')
