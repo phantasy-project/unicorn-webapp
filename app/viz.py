044c7353046f1ef3271b95b2fac9bfeb64a95dd6
Tong Zhang <zhangt@frib.msu.edu>
2018-01-30 15:55:41 -0500

    Add routine to plot hit trend figure.

--- app/viz.py
+++ app/viz.py
@@ -3,21 +3,25 @@
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models.tools import TapTool
-from bokeh.models.tools import HoverTool 
+from bokeh.models.tools import HoverTool
 from bokeh.models.tools import ResetTool
 from bokeh.models.tools import SaveTool
 from bokeh.models.tools import BoxZoomTool
 import numpy as np
+from datetime import datetime
 
 from .utils import eval_code
 
 
 TOOLS = [TapTool(), BoxZoomTool(),HoverTool(), ResetTool(), SaveTool(),]
 
-def create_plot(f):
-    # f: Function object
+def create_data_plot(f):
+    """Create plot of x [I] vs y [B/G].
+
+    f: Function object
+    """
     try:
-        p = figure(tools=TOOLS, plot_height=300, width=600)
+        p = figure(tools=TOOLS, plot_height=300, plot_width=600)
         x = f.get_x()
         y = f.get_y()
         xx = np.linspace(x.min(), x.max(), 100)
@@ -26,6 +30,29 @@ def create_plot(f):
                  alpha=0.8,
                  color='magenta')
         p.line(xx, yy, color='blue', alpha=0.6, line_dash='solid')
+        return components(p)
+    except:
+        return '<div></div>', '<div></div>'
+
+
+def create_trend_plot(f):
+    """Create plot for hit trend.
+
+    f: Function object
+    """
+    try:
+        x = f.get_hit_ts()
+        to_ts = np.vectorize(lambda x: (x - datetime(1970,1,1)).total_seconds())
+        from_ts = np.vectorize(lambda x: datetime.utcfromtimestamp(x))
+
+        hist, x_bins = np.histogram(to_ts(x))
+        x_bins = 0.5*(x_bins[0:-1] + x_bins[1:])
+        xx = from_ts(x_bins)
+        yy = hist
+
+        p = figure(tools=TOOLS, plot_height=300, plot_width=600,
+                   x_axis_type='datetime')
+        p.vbar(x=xx, top=yy, width=1.8e6, alpha=0.8)
         return components(p)
     except:
         return '<div></div>', '<div></div>'
