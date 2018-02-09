bfceee0a10e1258feb359c5f6681fa5af21b830a
Tong Zhang <zhangt@frib.msu.edu>
2018-02-09 15:25:54 -0500

    Add css and js files for table and function.

---
+++ static/js/table.js
@@ -0,0 +1,38 @@
+function format(d) {
+    return '<table class="table table-striped table-condensed">' +
+        '<tr>' +
+            '<td><strong>Function Definition</strong></td>' +
+            '<td><pre class="mypre" style="font-size:100%;">' + 
+            '<code class="python">' + d + '</code>' +
+            '</pre></td>' +
+        '</tr>' +
+    '</table>';
+}
+
+$(document).ready(function() {
+    var table = $("#func-all").DataTable({
+        "columns": [
+            null,null,null,null,
+            {'visible': true},
+            null,
+        ],
+        "order": [[ 1, "asec" ]],
+    });
+    
+    $("#func-all tbody").on('click', 'td.details-control', function() {
+        var tr = $(this).closest('tr');
+        var row = table.row(tr);
+        
+        if (row.child.isShown()) {
+            row.child.hide();
+            tr.removeClass('shown');
+        } else {
+            row.child(format(row.data()[4])).show();
+            tr.addClass('shown');
+
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        }
+    });
+});
