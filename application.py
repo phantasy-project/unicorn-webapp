3770184a03b79d56ea4f0d92612c77c1f3311bcd
Tong Zhang <zhangt@frib.msu.edu>
2018-03-09 15:24:16 -0500

    Add gitignore file.

--- application.py
+++ application.py
@@ -4,9 +4,4 @@ from app import app as application
 
 
 if __name__ == '__main__':
-    #application.run(host="0.0.0.0", debug=True)
-    application.run(host="0.0.0.0",
-                    #ssl_context='adhoc',
-                    #ssl_context=("cert.pem", "key.pem"),
-                    threaded=True,
-                    debug=True)
+    application.run(host="0.0.0.0", debug=True)
